96eb15153d820b44e25a3a310b524356704bdfab
BaldwinFaraday <otuquvopa34@gmail.com>
2025-11-07 16:38:42 +0800

    Add home page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { HabitTreeDemo } from "@/components/HabitTreeDemo";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-dark-primary">
+      <HabitTreeDemo />
+    </main>
+  );
+}
